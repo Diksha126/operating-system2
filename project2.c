72bfa4ad4889a15ea750c0080134a089b0645858
Diksha126 <62324089+Diksha126@users.noreply.github.com>
2020-04-03 15:35:30 +0530

    Update project2.c

--- project2.c
+++ project2.c
@@ -28,11 +28,11 @@ printf("error forking child process \n");
 exit(1);
 case 0:
 close(fd[1]);
-ssize_t child_bytes= read(fd[0],childbuffer,sizeof(childbuffer));
+ssize_t child_bytes= read(fd[0],childbuffer,sizeof(childbuffer));// reads from the source 
 close(fd[0]);
 
-int target=open("dest",O_CREAT|O_WRONLY);
-write(target,childbuffer,child_bytes);
+int target=open("dest",O_CREAT|O_WRONLY); 
+write(target,childbuffer,child_bytes); // write on the destination.
 
 default:
 close(fd[0]);
